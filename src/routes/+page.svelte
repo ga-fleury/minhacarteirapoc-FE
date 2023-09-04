78b6714e9e202afefbe5efb1111ce0b4334c1d1b
gafleury <gabrielfleury95@gmail.com>
2023-09-04 18:01:17 -0300

    if message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,21 +18,26 @@
 		}
 	}
 
-    function handleClick() {
+	function handleClick() {
 		apiResponse = roll();
 	}
 </script>
 
 <div class="flex flex-col justify-center items-center h-screen gap-y-[50px]">
 	<h1 class="text-3xl font-bold underline">Clique para fazer um request</h1>
-	<button class="text-2xl py-2 px-6 rounded shadow text-white bg-svelte-prime" on:click={handleClick}>
+	<button
+		class="text-2xl py-2 px-6 rounded shadow text-white bg-svelte-prime"
+		on:click={handleClick}
+	>
 		Beth?
 	</button>
-	{#await apiResponse}
-		<p>...waiting</p>
-	{:then body}
-		<p>{body.message}</p>
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
+	{#if apiResponse != ''}
+		{#await apiResponse}
+			<p>...waiting</p>
+		{:then body}
+			<p>{body.message}</p>
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	{/if}
 </div>
