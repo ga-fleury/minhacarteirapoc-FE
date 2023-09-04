25b74010eb261127835c07a34b18e8cde8e44e6f
gafleury <gabrielfleury95@gmail.com>
2023-09-04 17:22:17 -0300

    empty

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let apiResponse;
+	let apiResponse = '';
 
 	async function roll() {
 		const response = await fetch('https://q08tmqg247.execute-api.sa-east-1.amazonaws.com/dev/', {
@@ -17,11 +17,15 @@
 			throw new Error(apiResponse);
 		}
 	}
+
+    function handleClick() {
+		apiResponse = roll();
+	}
 </script>
 
 <div class="flex flex-col justify-center items-center h-screen">
 	<h1 class="text-3xl font-bold underline">Click button to make API request</h1>
-	<button class="text-2xl bg-svelte-prime-900 py-2 px-6 rounded shadow text-black" on:click={roll}>
+	<button class="text-2xl bg-svelte-prime-900 py-2 px-6 rounded shadow text-black" on:click={handleClick}>
 		Click Me
 	</button>
 	{#await apiResponse}
